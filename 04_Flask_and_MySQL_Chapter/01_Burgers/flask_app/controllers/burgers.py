de6a9a581b1ceea6214d07a0124c528dd849104f
BigSamu <valdesgutierrez@gmail.com>
2023-02-28 22:12:56 -0300

    code review 28/02/2023

--- 04_Flask_and_MySQL_Chapter/01_Burgers/flask_app/controllers/burgers.py
+++ 04_Flask_and_MySQL_Chapter/01_Burgers/flask_app/controllers/burgers.py
@@ -2,21 +2,58 @@ from flask import render_template, request, redirect
 from flask_app import app
 from flask_app.models.burger import Burger
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/create', methods=['POST'])
-def create():
-    data = {
-        "name": request.form['name'],
-        "bun": request.form['bun'],
-        "meat": request.form['meat'],
-        "calories": request.form['calories']
-    }
-    Burger.save(data)
-    return redirect('/burgers')
 
 @app.route('/burgers')
 def burgers():
-    return "Hello World"
+    burgers = Burger.get_all() 
+    return render_template('index.html', burgers = burgers)
+
+# @app.route('/burgers/new')
+# def new_burger():
+#     return render_template("new_burger.html")
+
+@app.route('/burgers/new', methods=['GET','POST'])
+def new_burger():
+    # POST REQUEST
+    if request.method == "POST":
+        data = {
+            "name": request.form['name'],
+            "bun": request.form['bun'],
+            "meat": request.form['meat'],
+            "calories": request.form['calories']
+        }
+        Burger.save(data)
+        return redirect(f'/burgers')
+    
+    # GET REQUEST
+    return render_template("new_burger.html")
+
+@app.route('/burgers/<int:burger_id>/edit', methods=['GET','POST'])
+def edit_burger(burger_id):
+    # POST REQUEST 
+    if request.method == 'POST':
+       data = {
+            'id': burger_id,
+            "name":request.form['name'],
+            "bun": request.form['bun'],
+            "meat": request.form['meat'],
+            "calories": request.form['calories']
+        }
+       Burger.update(data)
+       return redirect("/burgers")
+    
+    # GET REQUEST
+    else:   
+        data = {
+            'id': burger_id
+        }
+        burger = Burger.get_one(data)
+        return render_template("edit_burger.html", burger=burger)
+
+@app.route('/burgers/<int:burger_id>/delete')
+def delete_burger(burger_id):
+    data = {
+        'id': burger_id,
+    }
+    Burger.destroy(data)
+    return redirect('/burgers')
--- 04_Flask_and_MySQL_Chapter/01_Burgers/flask_app/models/burger.py
+++ 04_Flask_and_MySQL_Chapter/01_Burgers/flask_app/models/burger.py
@@ -18,12 +18,13 @@ class Burger:
     def get_all(cls):
         query =  "SELECT * FROM burgers;"
         results = connectToMySQL("burgers_db").query_db(query)
+
         burgers = []
         for burger in results:
             burgers.append(cls(burger))
         return burgers
-    
-        # Get One Burger
+
+    # Get One Burger
     @classmethod
     def get_one(cls, data):
         query = "SELECT * FROM burgers WHERE id=%(id)s;"
@@ -36,8 +37,18 @@ class Burger:
     def save(cls, data):
         query = "INSERT INTO burgers (name,bun,meat,calories,created_at,updated_at) VALUES (%(name)s,%(bun)s ,%(meat)s,%(calories)s,NOW(),NOW());"
         # data is a dictionary that will be passed into the save method from server.py
-        return connectToMySQL("burgers_db").query_db(query, data)
+        results = connectToMySQL("burgers_db").query_db(query, data) 
+        return results
     
     # 3) UPDATE OPERATIONS
-
+    # Edit Burger
+    @classmethod
+    def update(cls, data):
+        query = "UPDATE burgers SET name=%(name)s,bun=%(bun)s,meat=%(meat)s,calories=%(calories)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL("burgers_db").query_db(query, data)
+    
     # 4) DELETE OPERATIONS
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM burgers WHERE id = %(id)s;"
+        return connectToMySQL("burgers_db").query_db(query, data)
